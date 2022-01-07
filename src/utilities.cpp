3c45cb038a159159f682b88afee16bd470e229cf
M Joonas Pihlaja <jpihlaja@backtrace.io>
2022-01-07 20:17:41 +0000

    NFC sqlite_protobuf: Pull out TLS cache
    Summary:
    The data in the cached struct will be shared with `parse_message`
    in an upcoming commit.
    
    Ref Txxxx
    
    Depends on Dxxxx
    
    Test Plan: ran test scripts manually
    
    Reviewers: wandrews, pkhuong, ahicks
    
    Reviewed By: wandrews, pkhuong
    
    Maniphest Tasks: Txxxx
    
    Differential Revision: https://foo.backtrace.io/Dxxxx
    
    Signed-off-by: Paul Khuong <pkhuong@backtrace.io>

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -15,43 +15,53 @@ std::string string_from_sqlite3_value(sqlite3_value *value)
                         static_cast<size_t>(sqlite3_value_bytes(value)));
 }
 
+namespace {
+
+struct cache {
+    // Message type name or "".
+    std::string message_name;
+
+    // Cached prototype for the message type or `nullptr`.
+    const Message *prototype;
+};
+
+inline struct cache *get_cache() {
+    static thread_local struct cache cache;
+    return &cache;
+}
+
+} // namespace
+
 const Message *get_prototype(sqlite3_context *context,
                              const std::string& message_name)
 {
-    static thread_local struct {
-        // Message type name or "".
-        std::string message_name;
-
-        // Cached prototype for the message type or `nullptr`.
-        const Message *prototype;
-    } cached;
+    struct cache *cached = get_cache();
 
     // Look up the descriptor and prototype for the message type if necessary.
-    if (cached.prototype == nullptr || cached.message_name != message_name) {
-        cached.message_name = message_name;
+    if (cached->prototype == nullptr || cached->message_name != message_name) {
+        cached->message_name = message_name;
 
         const DescriptorPool *pool = DescriptorPool::generated_pool();
-        const Descriptor *descriptor = pool->FindMessageTypeByName(cached.message_name);
+        const Descriptor *descriptor = pool->FindMessageTypeByName(cached->message_name);
         if (descriptor) {
             MessageFactory *factory = MessageFactory::generated_factory();
-            cached.prototype = factory->GetPrototype(descriptor);
+            cached->prototype = factory->GetPrototype(descriptor);
         } else {
             sqlite3_result_error(context, "Could not find message descriptor", -1);
 
             // Invalidate the cache.
-            cached.message_name.clear();
-            cached.prototype = nullptr;
+            cached->message_name.clear();
+            cached->prototype = nullptr;
         }
     }
 
-    return cached.prototype;
+    return cached->prototype;
 }
 
 std::unique_ptr<Message> parse_message(sqlite3_context* context,
                                        const std::string& message_data,
                                        const std::string& message_name)
 {
-    // Lookup the prototype if necessary.
     const Message *prototype = get_prototype(context, message_name);
     if (!prototype)
         return nullptr;
